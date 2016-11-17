e23111c004264e9657abed31d1146074f076cb61
hardfist <yangjianzju@gmail.com>
2016-11-17 14:15:24 +0800

    test

--- hello.test.js
+++ hello.test.js
@@ -2,7 +2,7 @@
 
 var expect = require('chai').expect;
 var hello = require('./hello');
-
+console.log('test')
 describe('run hello.js', function() {
   it('should return "Hello World"', function() {
     expect(hello()).to.equal('Hello World');
